771357532df1586834fa61e1e3eeafca060b484f
Warwick Stone <uozu.aho@gmail.com>
2019-09-14 17:48:01 +1000

    flip card on press

--- lib/flashcard.dart
+++ lib/flashcard.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 
-class FlashCard extends StatelessWidget {
+class FlashCard extends StatefulWidget {
+  @override
+  _FlashCardState createState() => _FlashCardState();
+}
+
+class _FlashCardState extends State<FlashCard> {
+  final String _frontText = 'What is the word for 1?';
+  final String _backText = 'One';
+
+  bool _facingUp = true;
+  String _showingText;
+
   @override
   Widget build(BuildContext context) {
+    if (_showingText == null) _showingText = _frontText;
     return Center(
       child: Card(
         child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
           onTap: () {
-            print('Card tapped.');
+            setState(() {
+              _facingUp = !_facingUp;
+              _showingText = _facingUp ? _frontText : _backText;
+            });
           },
           child: Container(
             width: 300,
             height: 300,
             child: Center(
-              child: Text('What is the word for 1?'),
+              child: Text(_showingText)
             )
           ),
         ),
