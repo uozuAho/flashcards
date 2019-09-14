f699ff6477376f177ef323145ca4bfb43f664778
Warwick Stone <uozu.aho@gmail.com>
2019-09-14 19:03:03 +1000

    extract deck names from deck list widget

--- lib/widgets/deck_list.dart
+++ lib/widgets/deck_list.dart
@@ -2,20 +2,28 @@ import 'package:flashcards/widgets/flashcard.dart';
 import 'package:flutter/material.dart';
 
 class DeckList extends StatelessWidget {
+  final List<String> _deckNames = [
+    'deck 1',
+    'deck 2',
+    'deck 3',
+    'deck 4',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemCount: 10,
+        itemCount: _deckNames.length * 2,
         itemBuilder: (context, i) {
           if (i.isOdd) return Divider();
-          return _buildRow(context);
+          var deckIndex = i ~/ 2;
+          return _buildRow(context, deckIndex);
         });
   }
 
-  Widget _buildRow(BuildContext context) {
+  Widget _buildRow(BuildContext context, int deckIndex) {
     return ListTile(
-      title: Text('deck 1'),
+      title: Text(_deckNames[deckIndex]),
       onTap: () {
         Navigator.push(
           context,
