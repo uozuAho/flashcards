c2dea7cc526c0141470a6590dbda478e4562f009
Warwick Stone <uozu.aho@gmail.com>
2019-09-15 09:12:11 +1000

    extract decks page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flashcards/models/decks.dart';
-import 'package:flashcards/widgets/add_deck.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'widgets/deck_list.dart';
+import 'widgets/decks_page.dart';
 
 void main() => runApp(
     ChangeNotifierProvider(
@@ -18,26 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flash cards',
       home: DecksPage()
-    );
-  }
-}
-
-class DecksPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Decks'),
-      ),
-      body: DeckList(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => AddDeck()));
-        },
-      ),
     );
   }
 }
---
+++ lib/widgets/decks_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'add_deck.dart';
+import 'deck_list.dart';
+
+class DecksPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Decks'),
+      ),
+      body: DeckList(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AddDeck()));
+        },
+      ),
+    );
+  }
+}
