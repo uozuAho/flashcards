f085ac910f3d9d46b82b5d8481b97dfc9a6cca60
Warwick Stone <uozu.aho@gmail.com>
2019-09-15 09:28:11 +1000

    add deck page now works

--- lib/widgets/add_deck.dart
+++ lib/widgets/add_deck.dart
@@ -1,13 +1,42 @@
+import 'package:flashcards/models/decks.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class AddDeck extends StatelessWidget {
+class AddDeck extends StatefulWidget {
+  @override
+  _AddDeckState createState() => _AddDeckState();
+}
+
+class _AddDeckState extends State<AddDeck> {
+  final textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add new deck'),
-      )
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: textController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          var decks = Provider.of<DecksModel>(context);
+          decks.add(textController.text);
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.done),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
   }
 }
