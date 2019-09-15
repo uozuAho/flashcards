0cff4cea8281f7c838d33562aa141aaa751a3b93
Warwick Stone <uozu.aho@gmail.com>
2019-09-15 22:46:12 +1000

    move deck logic to business. much better!

--- lib/business/deck.dart
+++ lib/business/deck.dart
@@ -1,15 +1,30 @@
 class FlashcardDeck {
   final String name;
-  final List<Flashcard> cards;
+  final List<Flashcard> _cards;
+  int _topCardIndex = 0;
 
-  FlashcardDeck(this.name, this.cards);
+  FlashcardDeck(this.name, this._cards);
+
+  Flashcard get topCard => _cards[_topCardIndex];
+
+  void putTopCardOnBottom() {
+    _topCardIndex++;
+    if (_topCardIndex >= _cards.length) {
+      _topCardIndex = 0;
+    }
+  }
 }
 
 class Flashcard {
   final CardSide front;
   final CardSide back;
+  bool facingUp = true;
 
   Flashcard(this.front, this.back);
+
+  CardSide get showingSide => facingUp ? front : back;
+
+  void flip() => facingUp = !facingUp;
 }
 
 class CardSide {
--- lib/models/current_deck.dart
+++ lib/models/current_deck.dart
@@ -3,51 +3,23 @@ import 'package:flutter/material.dart';
 
 class CurrentDeck extends ChangeNotifier {
   FlashcardDeck _deck;
-  int _currentIndex = 0;
-  _CurrentCard _currentCard;
 
   String get deckName => _deck.name;
 
-  CardSide get showingCardSide => _currentCard.showingSide;
+  CardSide get showingCardSide => _deck.topCard.showingSide;
 
   void flipCard() {
-    _currentCard.flip();
+    _deck.topCard.flip();
     notifyListeners();
   }
 
   void nextCard() {
-    if (_currentIndex == _deck.cards.length - 1)
-      _setCardIndex(0);
-    else
-      _setCardIndex(_currentIndex + 1);
-
+    _deck.putTopCardOnBottom();
     notifyListeners();
   }
 
   void set(FlashcardDeck deck) {
     _deck = deck;
-    _setCardIndex(0);
     notifyListeners();
-  }
-
-  void _setCardIndex(int index) {
-    if (index >= _deck.cards.length) {
-      throw new Exception('index out of range');
-    }
-    _currentIndex = index;
-    _currentCard = _CurrentCard(_deck.cards[_currentIndex]);
-  }
-}
-
-class _CurrentCard {
-  final Flashcard _card;
-  bool _facingUp = true;
-
-  _CurrentCard(this._card);
-
-  CardSide get showingSide => _facingUp ? _card.front : _card.back;
-
-  void flip() {
-    _facingUp = !_facingUp;
   }
 }
