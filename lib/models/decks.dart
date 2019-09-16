61bd240f4bbebbc23631b3e1c813d9ca257ee81a
Warwick Stone <uozu.aho@gmail.com>
2019-09-16 20:05:21 +1000

    add russian alphabet

--- lib/models/decks.dart
+++ lib/models/decks.dart
@@ -18,7 +18,7 @@ class DecksModel extends ChangeNotifier {
 }
 
 final _testDataDecks = {
-  'cyrillic numbers': FlashcardDeck('cyrillic numbers', [
+  'Russian numbers': FlashcardDeck('Russian numbers', [
     Flashcard(CardSide('ноль'), CardSide('nol - 0')),
     Flashcard(CardSide('один'), CardSide('a\'deen - 1')),
     Flashcard(CardSide('два'), CardSide('dva - 2')),
@@ -30,5 +30,39 @@ final _testDataDecks = {
     Flashcard(CardSide('восемь'), CardSide('vo-syem - 8')),
     Flashcard(CardSide('девять'), CardSide('dyev-yat - 9')),
     Flashcard(CardSide('десять'), CardSide('dyes-ya - 10')),
+  ]),
+
+  'Russian alphabet': FlashcardDeck('Russian alphabet', [
+    Flashcard(CardSide('А'), CardSide('a in father')),
+    Flashcard(CardSide('Б'), CardSide('b')),
+    Flashcard(CardSide('В'), CardSide('v')),
+    Flashcard(CardSide('Г'), CardSide('g')),
+    Flashcard(CardSide('Д'), CardSide('d')),
+    Flashcard(CardSide('Е'), CardSide('e or ye - yes')),
+    Flashcard(CardSide('Ж'), CardSide('s in pleasure')),
+    Flashcard(CardSide('З'), CardSide('z')),
+    Flashcard(CardSide('И'), CardSide('i in police')),
+    Flashcard(CardSide('Й'), CardSide('y in toy')),
+    Flashcard(CardSide('К'), CardSide('k')),
+    Flashcard(CardSide('Л'), CardSide('l in lamp')),
+    Flashcard(CardSide('М'), CardSide('m')),
+    Flashcard(CardSide('Н'), CardSide('n')),
+    Flashcard(CardSide('О'), CardSide('o in more')),
+    Flashcard(CardSide('П'), CardSide('p')),
+    Flashcard(CardSide('Р'), CardSide('rolled r')),
+    Flashcard(CardSide('С'), CardSide('s in set')),
+    Flashcard(CardSide('Т'), CardSide('t')),
+    Flashcard(CardSide('У'), CardSide('oo in tool')),
+    Flashcard(CardSide('Ф'), CardSide('f')),
+    Flashcard(CardSide('Х'), CardSide('ch in bach')),
+    Flashcard(CardSide('Ц'), CardSide('ts in sits')),
+    Flashcard(CardSide('Ч'), CardSide('ch in chat')),
+    Flashcard(CardSide('Ш'), CardSide('sh in sharp')),
+    Flashcard(CardSide('Щ'), CardSide('sh in sheer')),
+    Flashcard(CardSide('Ы'), CardSide('i in hit')),
+    Flashcard(CardSide('Ь'), CardSide('soft sign')),
+    Flashcard(CardSide('Э'), CardSide('e in met')),
+    Flashcard(CardSide('Ю'), CardSide('yu in you\'ll')),
+    Flashcard(CardSide('Я'), CardSide('ya in yard')),
   ])
 };
